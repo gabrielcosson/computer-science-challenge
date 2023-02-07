0db240952cd4fe11f5e1f76d3c8cb8ddca43f9d2
gabrielnievescosson <gabrielnieves2000@gmail.com>
2023-02-06 20:25:47 -0500

    right or down availability
    This commit is just to save the progress I've made to the dance floor game algorithm. However I still need to find a way to get the longest possible sequence

--- dance_floor_endava.py
+++ dance_floor_endava.py
@@ -10,7 +10,45 @@ def readFile():
     matrix.pop(0)
     return matrix
     
+
+def isDownAvailable(matrix, x, y):    
+    if (x+1) < len(matrix):
+        if abs(matrix[x][y]-matrix[x+1][y]) == 1:
+            return True
+
+    return False
+
+
+def isRightAvailable(matrix, x, y):    
+    if (y+1) < len(matrix):
+        if abs(matrix[x][y]-matrix[x][y+1]) == 1:
+            return True
+
+    return False
     
+    
+def dance_floor(matrix, x, y):
+    sequence = []
+    dance_floor_algorithm(matrix, x, y, sequence)
+    return sequence
+
+
+def dance_floor_algorithm(matrix, x, y, sequence):
+    sequence.append(matrix[x][y])
+    if not isDownAvailable(matrix, x, y) and not isRightAvailable(matrix, x,y):
+        return
+    
+    if isDownAvailable(matrix, x, y):
+        dance_floor_algorithm(matrix, x+1, y, sequence)
+        return
+    
+    if isRightAvailable(matrix, x, y):
+        dance_floor_algorithm(matrix, x, y+1, sequence)
+        return
+            
+        
+
+
 if __name__ == "__main__":
     matrix = readFile()
-    print(matrix)
+    print(dance_floor(matrix, 1,1))
