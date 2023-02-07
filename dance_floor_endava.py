31ade54edd3b1bdc1786739fb1961bfc8e5c3e66
gabrielnievescosson <gabrielnieves2000@gmail.com>
2023-02-07 11:49:05 -0500

    modifying finding options algorithm
    with this implementation now its possible to get all the possible paths but JUST for a given starting point in the matrix

--- dance_floor_endava.py
+++ dance_floor_endava.py
@@ -28,26 +28,41 @@ def isRightAvailable(matrix, x, y):
     
     
 def dance_floor(matrix, x, y):
-    sequence = []
-    dance_floor_algorithm(matrix, x, y, sequence)
-    return sequence
+    options = [[]]
+    finding_options_algorithm(matrix, x, y, 0, options)
+    return options
+    
 
 
-def dance_floor_algorithm(matrix, x, y, sequence):
-    sequence.append(matrix[x][y])
-    if not isDownAvailable(matrix, x, y) and not isRightAvailable(matrix, x,y):
+def finding_options_algorithm(matrix, x, y, option_value, options):
+    options[option_value].append(matrix[x][y])
+
+    if isDownAvailable(matrix, x, y) and isRightAvailable(matrix, x,y):
+        options.append([])
+        options.append([])
+
+        option1 = len(options)-2
+        option2 = len(options)-1
+
+        for i in options[option_value]:
+            options[option1].append(i)
+            options[option2].append(i)
+
+        finding_options_algorithm(matrix, x+1, y, option1, options)
+        finding_options_algorithm(matrix, x, y+1, option2, options)
         return
     
     if isDownAvailable(matrix, x, y):
-        dance_floor_algorithm(matrix, x+1, y, sequence)
+        finding_options_algorithm(matrix, x+1, y, option_value, options)
         return
     
     if isRightAvailable(matrix, x, y):
-        dance_floor_algorithm(matrix, x, y+1, sequence)
+        finding_options_algorithm(matrix, x, y+1, option_value, options)
         return
+    
+    return
             
         
-
 
 if __name__ == "__main__":
     matrix = readFile()
