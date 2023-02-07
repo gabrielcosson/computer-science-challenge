33dc5d97cc646c150d7d2cab4c132844daf6d93f
gabrielnievescosson <gabrielnieves2000@gmail.com>
2023-02-07 16:09:32 -0500

    optimizing previous algorithm
    With this update now we don't have to iterate through all the items in the matrix, by removing all indexes that belong to a previous path because the previous path would be, by default, longer

--- dance_floor_endava.py
+++ dance_floor_endava.py
@@ -1,5 +1,7 @@
 import sys
 
+indexes = []
+
 def readFile():
     matrix = []
     txt = open("./test_files/DanceFloor01.txt", "r")
@@ -11,7 +13,7 @@ def readFile():
     return matrix
     
 
-def isDownAvailable(matrix, x, y):    
+def isRightAvailable(matrix, x, y):    
     if (x+1) < len(matrix):
         if abs(matrix[x][y]-matrix[x+1][y]) == 1:
             return True
@@ -19,7 +21,7 @@ def isDownAvailable(matrix, x, y):
     return False
 
 
-def isRightAvailable(matrix, x, y):    
+def isDownAvailable(matrix, x, y):    
     if (y+1) < len(matrix):
         if abs(matrix[x][y]-matrix[x][y+1]) == 1:
             return True
@@ -27,9 +29,24 @@ def isRightAvailable(matrix, x, y):
     return False
 
 
+def removing_used_items(removing_type, x, y):
+    if removing_type == "right":
+        if [x+1, y] in indexes:
+            indexes.remove([x+1,y])
+    elif(removing_type=="down"):
+        if [x, y+1] in indexes:
+            indexes.remove([x,y+1])
+    else:
+        if [x+1, y] in indexes:
+            indexes.remove([x+1,y])
+        if [x, y+1] in indexes:
+            indexes.remove([x,y+1])
+
+
 def find_longest_possibility(all_possibilities):
     length = 0
     max_path = 0
+
     for i in range(len(all_possibilities)):
         if len(all_possibilities[i])>length:
             max_path = i
@@ -43,7 +60,6 @@ def dance_floor(matrix, x, y):
     finding_options_algorithm(matrix, x, y, 0, options)
     return options
     
-
 
 def finding_options_algorithm(matrix, x, y, option_value, options):
     options[option_value].append(matrix[x][y])
@@ -59,16 +75,20 @@ def finding_options_algorithm(matrix, x, y, option_value, options):
             options[option1].append(i)
             options[option2].append(i)
 
+        removing_used_items("both", x, y)
+
         finding_options_algorithm(matrix, x+1, y, option1, options)
         finding_options_algorithm(matrix, x, y+1, option2, options)
         return
     
     if isDownAvailable(matrix, x, y):
-        finding_options_algorithm(matrix, x+1, y, option_value, options)
+        removing_used_items("down", x, y)
+        finding_options_algorithm(matrix, x, y+1, option_value, options)
         return
     
     if isRightAvailable(matrix, x, y):
-        finding_options_algorithm(matrix, x, y+1, option_value, options)
+        removing_used_items("right", x, y)
+        finding_options_algorithm(matrix, x+1, y, option_value, options)
         return
     
     return
@@ -79,11 +99,16 @@ if __name__ == "__main__":
     matrix = readFile()
     all_possibilities = []
 
-    for x in range(len(matrix)):
-        for y in range(len(matrix)):
-            options= dance_floor(matrix, x,y)
-            for i in options:
-                all_possibilities.append(i)
+    for y in range(len(matrix)):
+        for x in range(len(matrix)):
+            indexes.append([x,y])
+                
+    for value in indexes:     
+        options= dance_floor(matrix, value[0], value[1])
+        for i in options:
+            all_possibilities.append(i)
     
-    print("Longest Endavans Line Dance is: ",*find_longest_possibility(all_possibilities))
-    print("Length of Path is: ",len(find_longest_possibility(all_possibilities)))
+
+    result = find_longest_possibility(all_possibilities)
+    print("Longest Endavans Line Dance is: ", *result)
+    print("Length of Path is: ", len(result))
