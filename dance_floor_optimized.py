057a7cf1e86b21efaa9f4260edc5593698ae2dac
gabrielnievescosson <gabrielnieves2000@gmail.com>
2023-02-08 14:48:21 -0500

    improving program structure

--- dance_floor_optimized.py
+++ dance_floor_optimized.py
@@ -1,80 +1,69 @@
 def readFile(fileName):
     matrix = []
     txt = open("./test_files/"+fileName, "r")
+    size = int(txt.readline())
     
-    for x in txt:
-        for value in list(map(int,x.split())):
-            matrix.append(value)
+    for _ in range(size):
+        matrix.extend(list(map(int, txt.readline().split())))
 
-    size = matrix.pop(0)
     return (size, matrix)
 
 
-def determining_connections(size, matrix):
-    available_routes = [[] for _ in range(len(matrix))]
-    iterator_per_line = 0
+def finding_connections(size, matrix):
+    connections = [[] for _ in range(len(matrix))]
+    column = 0
     
-    for x in range(len(available_routes)):
-        #linear matrix border check
-        corner_check = False
+    for x in range(len(connections)):
+        max_column = False
 
-        if iterator_per_line == size-1:
-            corner_check = True
+        if column == size-1:
+            max_column = True
 
-        #right available 
-        if (x+1) < len(matrix) and not corner_check:
+        #Right path available 
+        if (x+1) < len(matrix) and not max_column:
             if abs(matrix[x] - matrix[x+1]) == 1:
-                available_routes[x].append(x+1)
+                connections[x].append(x+1)
         
-        #down available
+        #Down path available
         if (x+size) < len(matrix):
             if abs(matrix[x] - matrix[x+size]) == 1:
-                available_routes[x].append(x+size)
+                connections[x].append(x+size)
         
-        iterator_per_line+=1
-        if corner_check:
-            iterator_per_line = 0
+        column+=1
+        
+        if max_column:
+            column = 0
 
-    return available_routes
+    return connections
 
 
-def finding_longest_path(available_routes):
-    options = [[] for _ in range(len(available_routes))]
+def longest_path(connections):
+    options = [[] for _ in range(len(connections))]
 
-    for x in range(len(available_routes) -1, -1, -1):
-        #there are 2 possible paths
-        if len(available_routes[x]) == 2:
-            options[x].append(x)
-            if len(options[available_routes[x][0]]) > len(options[available_routes[x][1]]):
-                options[x].extend(options[available_routes[x][0]])
+    for x in range(len(connections) -1, -1, -1):
+        options[x].append(x)
+
+        #There are two available paths
+        if len(connections[x]) == 2:
+            if len(options[connections[x][0]]) > len(options[connections[x][1]]):
+                options[x].extend(options[connections[x][0]])
             else:
-                options[x].extend(options[available_routes[x][1]])
-
-        #there is just 1 path
-        elif(len(available_routes[x]) == 1):
-            options[x].append(x)
-            options[x].extend(options[available_routes[x][0]])
+                options[x].extend(options[connections[x][1]])
         
-        #there is no path
-        else:
-            options[x].append(x)
+        #There is just one available path
+        elif(len(connections[x]) == 1):
+            options[x].extend(options[connections[x][0]])
 
-    return options
-
-
-def printing_longest_line(matrix, options):
-    longest_line = []
-    longest_indexes = max(options, key=len)
-    print(longest_indexes)
-    
-    for i in longest_indexes:
-        longest_line.append(matrix[i])
-    
-    print("Longest Endavans Line Dance is: ", *longest_line)
-    print("Length of Path is: ", len(longest_line))
+    return max(options, key=len)    
 
 
 if __name__ == "__main__":
-    size, matrix = readFile("DanceFloor06.txt")
-    options = finding_longest_path(determining_connections(size, matrix))
-    printing_longest_line(matrix, options)
+    longest_dancing_line = []
+    size, matrix = readFile("DanceFloor07.txt")
+    indexes = longest_path(finding_connections(size, matrix))
+    
+    for i in indexes:
+        longest_dancing_line.append(matrix[i])
+    
+    print("Longest Endavans Line Dance is: ", *longest_dancing_line)
+    print("Length of Path is: ", len(longest_dancing_line))
