026ebd347733caeb48d7b762a7c2cfdd6385426e
gabrielnievescosson <gabrielnieves2000@gmail.com>
2023-02-08 09:50:34 -0500

    creating the connections matrix

--- dance_floor_optimized.py
+++ dance_floor_optimized.py
@@ -13,9 +13,22 @@ def readFile(fileName):
     return (size, matrix)
 
 
+def determining_connections(size, matrix):
+    available_routes = [[] for _ in range(len(matrix))]
+    for x in range(len(available_routes)):
+        #right available 
+        if (x+1) < len(matrix):
+            if abs(matrix[x] - matrix[x+1]) == 1:
+                available_routes[x].append(x+1)
+        #down available
+        if (x+size) < len(matrix):
+            if abs(matrix[x] - matrix[x+size]) == 1:
+                available_routes[x].append(x+size)
+
+    return available_routes
 
 
 
 if __name__ == "__main__":
     size, matrix = readFile("DanceFloor01.txt")
-    print(matrix)
+    print(determining_connections(size, matrix))
